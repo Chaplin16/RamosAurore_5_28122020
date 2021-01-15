bd7ee10700e0e0ff31b254466638330d2290026e
Aurore RAMOS <chaplin16@orange.fr>
2021-01-15 20:28:08 +0000

    main-index.html et etoile-cards

---
+++ P5FrontEnd/objectTeddyBears.js
@@ -0,0 +1,16 @@
+//objet constructor function
+class TeddyBears {
+    constructor(product) {
+        this.name = product.name;
+        this.imageUrl = product.imageUrl;
+        this.price = product.price;
+        this.id = product._id;
+        this.description = product.description;
+    }
+
+}
+    
+
+
+
+
