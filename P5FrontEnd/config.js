d5f968ff906a9db78363a172f5fc7a0db0e46355
Aurore RAMOS <chaplin16@orange.fr>
2021-01-15 20:28:08 +0000

    main-index.html et etoile-cards

--- P5FrontEnd/config.js
+++ P5FrontEnd/config.js
@@ -2,3 +2,6 @@ var config = {
     host:"http://localhost:3000/api/",
     api: "teddies"
 }
+//teddies
+//furniture
+//cameras
--- P5FrontEnd/showProductIndex.js
+++ P5FrontEnd/showProductIndex.js
@@ -1,21 +1,106 @@
+/* let listProductData = [];
+let containerProduct = document.getElementById('listProduct');
+let teddy = "";
 
-// je recupere la photo de la liste
+function showProduct(product) {
+    for (let product of listProductData) {
+            teddy = new TeddyBears(product);
+            containerProduct.innerHTML += showProduct(teddy);
+        };
+        
+      
+    return `<div class="col-12 col-lg-4">
+    <div class="card border-warning border-5 m-5">
+        <div id="cardProduct" class="card-body ">
+            <h2 class="card-title">${product.name}</h2>
+            <a href="productDetail.html" id="${product.id}>
+                <figure id="figure">
+                    <p id="erreur"></p>  
+                    <img src=${product.imageUrl} class="img-fluid rounded align-items-center">
+                </figure>
+                <p class="text-right font-weight-bold">Prix:${product.price / 100}€</p>
+            </a>    
+        </div>
+    </div>
+    </div>` 
+} 
 fetch(config.host + config.api)
-    .then(response => {
-        if(response.ok) { 
-            response.json().then(listProductData => {
-            const containerProduct = document.getElementById('listProduct');
 
-                for (let product of listProductData) {
-                    let teddy = new TeddyBears(product);
-                    containerProduct.innerHTML += teddy.display();
-                }
-            })
-        }else {
-            console.log("ERREUR D AFFICHAGE")
-            document.getElementById('erreur').innerHTML = "Oups!Il y a un soucis avec l'affichage de la jolie peluche :-("
+.then(response => {
+    response.json().then(listProductData => {
+        containerProduct.innerHTML += showProduct(teddy);
+    }) 
+})
+.catch((err) => {
+    console.log("ERREUR D AFFICHAGE");
+    document.getElementById('erreur').innerHTML = " Oups! erreur d affichage :-( ";
+});     
+*/
+
+/*  function showProduct(product) {
+    return `<div class="col-12 col-lg-4">
+    <div class="card border-warning border-5 m-5">
+        <div id="cardProduct" class="card-body ">
+            <h2 class="card-title">${product.name}</h2>
+                <a href="productDetail.html/#5beaa8bf1c9d440000a57d94></a>
+                <figure id="figure">
+                    <p id="erreur"></p>
+                    <img src=${product.imageUrl} class="img-fluid rounded align-items-center">
+                </figure>
+                <p class="text-right font-weight-bold">Prix:${product.price / 100}€</p>
+            </a>
+        </div>
+    </div>
+</div>`
+} */
+
+
+
+let cardProduct = document.getElementById('cardProduct');
+let containerProduct = document.getElementById('listProduct'); 
+let mainDetail = document.getElementById('mainDetail');
+let teddy = "";
+
+// je recupere l adresse api via la variable config
+fetch(config.host + config.api)
+//la promesse
+.then(response => {
+//le parametre listProductData c est le array
+    response.json().then(listProductData => {
+         console.log(listProductData);
+//la boucle for of qui incremente
+        for (let product of listProductData) {
+            teddy = new TeddyBears(product);
+//la main en html            
+            containerProduct.innerHTML += 
+             `<div class="col-12 col-lg-4">
+                <div class="card border-warning border-5 m-5">
+                    <div id="cardProduct" class="card-body ">
+                        <h2 class="card-title">${product.name}</h2>
+                        <a href="productDetail.html?id=${product._id} ">
+                            <figure id="figure">
+                                <p id="erreur"></p>
+                                <img src=${product.imageUrl} class="img-fluid rounded align-items-center" alt="ours en peluche">
+                            </figure>
+                            <figcaption>
+                            <p class="text-right font-weight-bold">Prix:${product.price / 100}€</p>
+                            <img src="star-solid.jpg"/>
+                            <img src="star-solid.jpg"/>
+                            <img src="star-solid.jpg"/>
+                            <img src="star-solid.jpg"/>
+                            <img src="star-solid.jpg"/>
+                        </a>
+                        <button
+                    </div>
+                </div>
+            </div>`
         }
-    }
-)
+    })
+})
+//le retour en cas de non connection au serveur 
+.catch((err) => {
+    console.log("ERREUR D AFFICHAGE");
+    document.getElementById('erreur').innerHTML = " Oups! Il ///y a un soucis avec l'affichage de la jolie peluche :-( ";
 
+});
 
