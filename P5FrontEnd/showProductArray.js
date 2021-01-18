6662097cee18b24239840b5b5bb61fa8d259234d
Aurore RAMOS <chaplin16@orange.fr>
2021-01-18 15:35:46 +0000

    affichage produit en detail

--- P5FrontEnd/showProductArray.js
+++ P5FrontEnd/showProductArray.js
@@ -1,73 +1,17 @@
-/* let listProductData = [];
-let containerProduct = document.getElementById('listProduct');
-let teddy = "";
-
-function showProduct(product) {
-    for (let product of listProductData) {
-            teddy = new TeddyBears(product);
-            containerProduct.innerHTML += showProduct(teddy);
-        };
-        
-      
-    return `<div class="col-12 col-lg-4">
-    <div class="card border-warning border-5 m-5">
-        <div id="cardProduct" class="card-body ">
-            <h2 class="card-title">${product.name}</h2>
-            <a href="productDetail.html" id="${product.id}>
-                <figure id="figure">
-                    <p id="erreur"></p>  
-                    <img src=${product.imageUrl} class="img-fluid rounded align-items-center">
-                </figure>
-                <p class="text-right font-weight-bold">Prix:${product.price / 100}€</p>
-            </a>    
-        </div>
-    </div>
-    </div>` 
-} 
-fetch(config.host + config.api)
-
-.then(response => {
-    response.json().then(listProductData => {
-        containerProduct.innerHTML += showProduct(teddy);
-    }) 
-})
-.catch((err) => {
-    console.log("ERREUR D AFFICHAGE");
-    document.getElementById('erreur').innerHTML = " Oups! erreur d affichage :-( ";
-});     
-*/
-
-/*  function showProduct(product) {
-    return `<div class="col-12 col-lg-4">
-    <div class="card border-warning border-5 m-5">
-        <div id="cardProduct" class="card-body ">
-            <h2 class="card-title">${product.name}</h2>
-                <a href="productDetail.html/#5beaa8bf1c9d440000a57d94></a>
-                <figure id="figure">
-                    <p id="erreur"></p>
-                    <img src=${product.imageUrl} class="img-fluid rounded align-items-center">
-                </figure>
-                <p class="text-right font-weight-bold">Prix:${product.price / 100}€</p>
-            </a>
-        </div>
-    </div>
-</div>`
-} */
-
-
-
 let cardProduct = document.getElementById('cardProduct');
 let containerProduct = document.getElementById('listProduct'); 
 var myFetch = fetch(config.host + config.api)
 // je recupere l adresse api via la variable config
 
-function showProduct(product) {
+function showProduct(product) { 
+    let url = "productDetail.html?id=" + product.id
+
     containerProduct.innerHTML += 
     `<div class="col-12 col-lg-4">
     <div class="card border-warning border-5 m-5">
         <div id="cardProduct" class="card-body ">
             <h2 class="card-title">${product.name}</h2>    
-            <a href="productDetail.html?id=${product._id}">
+            <a href= ${url}>
                 <figure id="figure">
                     <p id="erreur"></p>
                     <img src=${product.imageUrl} class="img-fluid rounded align-items-center" alt="ours en peluche">
@@ -93,8 +37,10 @@ myFetch.then(function (response) {
     .then(function(listProductData) {
        
         for (let product of listProductData) {
-            console.log(listProductData)
+
+
             let teddy = new TeddyBears(product);
+            
         showProduct(teddy);
         }
     })
--- P5FrontEnd/showProductDetail.js
+++ P5FrontEnd/showProductDetail.js
@@ -1,24 +1,76 @@
 
-var myFetch = fetch(config.host + config.api)
 let containerProductDetail = document.getElementById('containerProductDetail');
 const urlParams = new URL(document.location).searchParams;
 const id = urlParams.get("id");
 
-console.log(id);
 
-let itemId = {
-    method:"GET/:_id"
-}
+fetch("http://localhost:3000/api/teddies/" + id)
+  .then(function(response) { console.log(response)
+      return response.json()    
+  })
+  .then(function(showTeddy){
+        containerProductDetail.innerHTML = 
+        `<section class="flex row justify-content-center">
+        <div class="col-5 border card border-warning">                    
+            <figure id="figure">
+               <p id="erreur"></p>
+               <img src=${showTeddy.imageUrl} class="img-fluid rounded align-items-center" alt="ours en peluche">
+            </figure>
+        </div>  
+        <div id="card-body" class=" col-5 border  flex flex-col flex-wrap bg-light">
+            <h2 class="card-title text-center mt-3">${showTeddy.name}</h2>    
+           <figcaption>
+               <p class="card-text text-center">${showTeddy.descritpion}</p>
+            </figcaption>  
+            <div class="form">
+                <p class="lead font-weight-bold">Si vous voulez me personnaliser</p>
+                <div class="form-check">
+                    <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="origine" value="color" checked>
+                    couleur rouge
+                    </label>
+                </div>
+                <div class="form-check">
+                    <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="origine" value="color">
+                    color vert 
+                    </label>
+                </div>
+                <div class="form-check">
+                    <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="origine" value="color">
+                    color jaune
+                    </label>
+                </div>
+                <div class="form-check">
+                    <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="origine" value="color">
+                    color bleu
+                    </label>
+                </div> 
+            </div>    
+            <p class="text-right font-weight-bold">Prix:${showTeddy.price / 100}€</p>
+            <img src="star-solid.jpg"/>
+            <img src="star-solid.jpg"/>
+            <img src="star-solid.jpg"/>
+            <img src="star-solid.jpg"/>
+            <img src="star-solid.jpg"/> 
+           <button onclick="window.location.href ="#" type="button" value="Commander" class="float-right">Commander</button>
+        </div>
+    </div>`
+
+  })
 
 
-fetch("http://localhost:3000/api/teddies",itemId)
+
+/* fetch("http://localhost:3000/api/teddies",itemId)
     .then(function (response) {
     return response.json()
-    })
+   })
     .then(function(showItem){
-    let teddy = new TeddyBears(product);
-    containerProductDetail.innerHTML
+   
+   containerProductDetail.innerHTML
     })
 //.catch(error => alert("Erreur : " + error));
-
+ */
    
