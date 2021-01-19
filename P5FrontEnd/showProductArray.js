56af67ac1682c8e3be215632e574f2a4c776c2c0
Aurore RAMOS <chaplin16@orange.fr>
2021-01-19 16:08:53 +0000

    fiche produit avec option et panier en html

--- P5FrontEnd/showProductArray.js
+++ P5FrontEnd/showProductArray.js
@@ -1,22 +1,27 @@
-let cardProduct = document.getElementById('cardProduct');
+//creation des variables
+
+// variable où je placerai le html en js
 let containerProduct = document.getElementById('listProduct'); 
+//variable de l url de api avec choix du produit dans config.api
 var myFetch = fetch(config.host + config.api)
-// je recupere l adresse api via la variable config
 
+
+
+// fonction generique (affichage de tous les produits))
 function showProduct(product) { 
+// je recupere l url où j y rajoute l id de la peluche cliquée
     let url = "productDetail.html?id=" + product.id
-
+// je rajoute le HTML dans le js avec les variables de toutes les propriétés
     containerProduct.innerHTML += 
     `<div class="col-12 col-lg-4">
-    <div class="card border-warning border-5 m-5">
-        <div id="cardProduct" class="card-body ">
+    <div class="card border-warning border-5 m-4">
+        <div class="card-body ">
             <h2 class="card-title">${product.name}</h2>    
             <a href= ${url}>
                 <figure id="figure">
                     <p id="erreur"></p>
                     <img src=${product.imageUrl} class="img-fluid rounded align-items-center" alt="ours en peluche">
                 </figure>
-            
                 </a>    
                 <p class="text-right font-weight-bold">Prix:${product.price / 100}€</p>
                 <img src="star-solid.jpg"/>
@@ -28,26 +33,28 @@ function showProduct(product) {
             <button onclick="window.location.href ="#" type="button" value="Commander" class="text-right float-right">Commander</button>
         </div>
     </div>
-    </div>`
+    </div>`;
     }
 
+// requete vers l api
 myFetch.then(function (response) {
+//premiere promesse avec la reponse json
     return response.json()
     })
+//promesse de la reponse json a qui je demande la liste des produits
     .then(function(listProductData) {
-       
+
+//j utilise la boucle for of pour recuperer un produit de la liste
         for (let product of listProductData) {
-
-
+// je lui cree teddy sur le modele du constructor TeddyBear
             let teddy = new TeddyBears(product);
-            
+// j appelle ma fonction de la ligne 6 en lui passant en parametre teddy            
         showProduct(teddy);
         }
-    })
 
-       
+    })       
 //le retour en cas de non connection au serveur 
-//.catch(function(err) => {
-//    console.log('Fetch problem: ' + err.message);
+//.catch(function(err) {
+ //   console.log('Fetch problem: ' + err.message);
 //});
 
