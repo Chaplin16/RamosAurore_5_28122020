e9cf7aed054b03d2ef30df4a6f01d24e5328ab55
Aurore RAMOS <chaplin16@orange.fr>
2021-01-21 21:14:35 +0000

    quantite affichee dans le header et bouton avec infos et variables

---
+++ P5FrontEnd/orderProduct.js
@@ -0,0 +1,36 @@
+// // //creation des variables
+
+//variable de l endroit où je recupere les quantités
+// let quantity = document.getElementsByClassName("quantity")[0];
+// console.log(document.getElementsByClassName("quantity"))
+// let resultChoiseQuantity = document.getElementsByClassName("resultChoiseQuantity");
+// console.log(quantity)
+// // creation ecouteur d evenement avec recuperation des quantités pour affichage dans le panier
+// quantity.addEventListener("input", function(event){
+//     console.log(event)
+//     resultChoiseQuantity.innertext = event.target.value;
+//     console.log(resultChoiseQuantity)
+//   });
+ 
+
+
+// //variable pour recuperer les infos du boutton
+// let btnAddToCart = document.getElementsByClassName('addToCart');
+
+// console.log(btnAddToCart)
+
+// let teddyPrice = btnAddToCart.getAttribute("data-price");
+// let teddyId = btnAddToCart.getAttribute('id');
+// let teddyName = btnAddToCart.getAttribute('data-name');
+// console.log(teddyPrice)
+// console.log(teddyId)
+// console.log(teddyName)
+
+
+  // btnAddToCart.click(function() {
+
+  //   let $this = $(this);
+  //   let name = $(this).data('data-name');
+  //   let price = Number($(this).data('data-price / 100'));
+  // console.log(name)
+  //  }); 
--- P5FrontEnd/showProductDetail.js
+++ P5FrontEnd/showProductDetail.js
@@ -16,7 +16,7 @@ fetch("http://localhost:3000/api/teddies/" + id)
 //je cree une fonction pour recuperer les donnees de cette url
     .then(function (showTeddy) {
        
-// je recupere les infos precis du nounours (nom, image, prix, description )
+        // je recupere les infos precis du nounours (nom, image, prix, description )
         containerProductDetail.innerHTML +=
             `<section class="flex row justify-content-center mt-3 mb-3">
             <div class="col-5 border card border-warning border-right-0">                    
@@ -37,7 +37,7 @@ fetch("http://localhost:3000/api/teddies/" + id)
                       </div>  
                 </div>    
                 <p class="text-right font-weight-bold">Prix:${showTeddy.price / 100}€</p>
-                <p id="quantity-wanted" class="text-right">
+                <form class="text-right">
                     <label>Quantité</label>
                     <select id="quantity" name="quantite">
                         <option value="1">1</option>
@@ -51,20 +51,52 @@ fetch("http://localhost:3000/api/teddies/" + id)
                         <option value="9">9</option>
                         <option value="10">10</option>
                     </select>
-                </p>
+                </form>
                 <img src="star-solid.jpg"/>
                 <img src="star-solid.jpg"/>
                 <img src="star-solid.jpg"/>
                 <img src="star-solid.jpg"/>
                 <img src="star-solid.jpg"/> 
-                <button onclick="window.location.href ="#" type="button"class="add-to-cart float-right mb-4 btn btn-warning font-weight-bold border-dark" data-id="${showTeddy._id}" data-name="${showTeddy.name}" data-price="${showTeddy.price /100}"  value="Commander">Ajouter au panier</button>
+                <input type="submit" onclick= "window.location.href ='orderProduct.html';" id="addToCart" class="float-right mb-4 btn btn-warning font-weight-bold border-dark" data-id="${showTeddy._id}" data-name="${showTeddy.name}" data-price="${showTeddy.price /100}" value="Commander"></button>
             </div>
         </section>`;
+        
+//AFFICHER LA QUANTITE DE PRODUIT A ACHETER DANS LE PANIER
+        //variable de l endroit où je recupere les quantités
+        let quantity = document.getElementById("quantity");  
+             console.log(quantity) 
+           
+        let quantityInCart = document.getElementById("quantity-in-cart");
+            console.log(quantityInCart)//<p id="quantity-in-cart"></p>
+        
+        // creation ecouteur d evenement avec recuperation des quantités pour affichage dans le panier 
+       
+         quantity.addEventListener("input", function() { 
+            quantityInCart.innerHTML = `<span>${quantity.value}</span> articles`;
+           
+            
+         });
 
+ //RECUPERER LES INFOS DU BOUTON
+        let btnAddToCart = document.getElementById("addToCart");
+            
+        console.log(btnAddToCart) // <input type="submit" onclick= "window.location.href ='orderProduct.html';" id="addToCart" class="float-right mb-4 btn btn-warning font-weight-bold border-dark" data-id="5be9c8541c9d440000665243" data-name="Norbert" data-price="29" value="Commander"></button>
+            
+        let teddyPrice = btnAddToCart.dataset.price;
+        let teddyId = btnAddToCart.dataset.id;
+        let teddyName = btnAddToCart.dataset.name;
+
+        console.log(teddyPrice) // 29
+        console.log(teddyId) // 5be9c8541c9d440000665243
+        console.log(teddyName) // Norbert
+
+            
+
+//AFFICHER LES OPTIONS DE COULEURS        
 // je cree ma variable d options de couleurs 
         let stringOptionColor = ""; 
 
-// j utilise une boucle for pour incrementer mes couleurs
+// j utilise une boucle for pour notifier chaque couleur
         for (let color of showTeddy.colors) {
              stringOptionColor += `<option value="${color}">${color}</option>`
          }
@@ -80,5 +112,5 @@ fetch("http://localhost:3000/api/teddies/" + id)
             <select id="clr" name="color">
             ${stringOptionColor}</select>`
  
-    })
-
+    });
+    
