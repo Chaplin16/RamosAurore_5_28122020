37018f32654e57c24a4b263fbf4c094b949800dd
Aurore RAMOS <chaplin16@orange.fr>
2021-01-24 17:33:47 +0000

    quantity(in header) et color in order

--- P5FrontEnd/orderProduct.js
+++ P5FrontEnd/orderProduct.js
@@ -23,8 +23,9 @@
         <tr class="bg-white">
           <td>${firstname}</td>
           <td class="text-left pl-5">${price}€</td>
+          <td>${color}</td>
           <td id="resultChoiseQuantity" class="text-left pl-5">${quantity}</td>
-          <td><img class="ml-4 h-75" src="trash-can_38501.png"></td>
+          <td><img id="trash" class="ml-4 h-75" src="trash-can_38501.png"></td>
           <td class="text-center">${total}€</td>
         </tr>
         <tr class="bg-white">
@@ -32,5 +33,18 @@
           <td></td>
           <td></td>
           <td></td>
+          <td></td>
           <td class="text-center">${total}€</td>
         </tr>`;
+
+//quand on clique sur la poubelle, la ligne s efface 
+      let trash = document.getElementById("trash");
+
+      trash.addEventListener('click', function() {
+        cartTableBody.remove
+      })
+
+// afficher la quantite total d articles voulues
+let quantityInCart = document.getElementById("quantity-in-cart"); 
+
+quantityInCart.innerHTML = `<span>${quantity}</span> articles`; 
