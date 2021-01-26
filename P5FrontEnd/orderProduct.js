27f01627e09bbb563c5ddfe02cc349301bac3ec1
Aurore RAMOS <chaplin16@orange.fr>
2021-01-26 20:11:23 +0000

    modication innerHTML de orderProdcut.js

--- P5FrontEnd/orderProduct.js
+++ P5FrontEnd/orderProduct.js
@@ -13,13 +13,14 @@ if (panier) {
     alert("le panier est vide")
 }
 console.log(panier)
-//Boucle pour importer données de chaque article panier
-function ligneTableau() {
-    panier.forEach((basket) => {
- 
-    let total = 0;      
-    total += basket.price * basket.quantity;
+ //Boucle pour importer données de chaque article panier
 
+
+function ligneTableau() {
+let total = 0;
+
+    panier.forEach((basket) => {
+    total += basket.price * basket.quantity;   
     cartTableBody.innerHTML += `
             <tr class="bg-white">
             <td>${basket.firstName}</td>
@@ -28,17 +29,18 @@ function ligneTableau() {
             <td id="resultChoiseQuantity" class="text-left pl-5">${basket.quantity}</td>
             <td><img id="trash" class="ml-4 h-75" src="trash-can_38501.png"></td>
             <td class="text-center">${total}€</td>
-          </tr>
-          <tr class="bg-white">
+          </tr>`;  
+    }); 
+
+    cartTableBody.innerHTML += 
+          `<tr class="bg-white">
             <td>Prix total</td>
             <td></td>
             <td></td>
             <td></td>
             <td></td>
             <td class="text-center">${total}€</td>
-          </tr>`;
-    })
-
+          </tr>`;  
 };
 
 
