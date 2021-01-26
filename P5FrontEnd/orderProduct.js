b462fcc32f093fdccb866fe2a384903ed16099b5
Aurore RAMOS <chaplin16@orange.fr>
2021-01-26 17:23:50 +0000

    essai pour affichage plusieurs lignes commandes

--- P5FrontEnd/orderProduct.js
+++ P5FrontEnd/orderProduct.js
@@ -1,60 +1,58 @@
-///recuuperer les infos de
-let idItem = localStorage.getItem('idItem')
-let firstname = localStorage.getItem('firstname')
-let price = localStorage.getItem('price')
-let quantity = localStorage.getItem('quantity')
-let color = localStorage.getItem('color')
+//recuperer les infos de la commande 
+const panier = JSON.parse(localStorage.getItem("basket"));
 
+//je recupere le lieu d inplantation
 let cartTableBody = document.getElementById("cart-tablebody");
-let shopping = [];
-let cardTeddy = {
-  idItem: idItem,
-  firstname: firstname,
-  price: price,
-  color: color,
-  quantity: quantity
-}
+cartTableBody.innerHTML ="";
 
-shopping.push(cardTeddy);
 
-shopping.forEach((cardTeddy) => {
-    if(shopping != null) {
-      shopping.push(cardTeddy)
+//Condition pour afficher le panier
+if (panier) {
+    ligneTableau(); //appelle la fonction que tu trouveras ci-dessous
     } else {
-    shopping = [];
-    }
+    alert("le panier est vide")
+}
+console.log(panier)
+//Boucle pour importer données de chaque article panier
+function ligneTableau() {
+    panier.forEach((basket) => {
+ 
+    let total = 0;      
+    total += basket.price * basket.quantity;
+
+    cartTableBody.innerHTML += `
+            <tr class="bg-white">
+            <td>${basket.firstName}</td>
+            <td class="text-left pl-5">${basket.price}€</td>
+            <td>${basket.color}</td>
+            <td id="resultChoiseQuantity" class="text-left pl-5">${basket.quantity}</td>
+            <td><img id="trash" class="ml-4 h-75" src="trash-can_38501.png"></td>
+            <td class="text-center">${total}€</td>
+          </tr>
+          <tr class="bg-white">
+            <td>Prix total</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="text-center">${total}€</td>
+          </tr>`;
+    })
+
+};
 
 
-cartTableBody.innerHTML = "";
-let total = 0;
-total += price * quantity;
 
-cartTableBody.innerHTML += `
-        <tr class="bg-white">
-          <td>${cardTeddy.firstname}</td>
-          <td class="text-left pl-5">${cardTeddy.price}€</td>
-          <td>${cardTeddy.color}</td>
-          <td id="resultChoiseQuantity" class="text-left pl-5">${cardTeddy.quantity}</td>
-          <td><img id="trash" class="ml-4 h-75" src="trash-can_38501.png"></td>
-          <td class="text-center">${total}€</td>
-        </tr>
-        <tr class="bg-white">
-          <td>Prix total</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td class="text-center">${total}€</td>
-        </tr>`;
 
-//quand on clique sur la poubelle, la ligne s efface 
-let trash = document.getElementById("trash");
+ 
 
-trash.addEventListener('click', function () {
-  cartTableBody.remove
-})
 
-// afficher la quantite total d articles voulues
-let quantityInCart = document.getElementById("quantity-in-cart");
 
-});
+// //quand on clique sur la poubelle, la ligne s efface 
+// let trash = document.getElementById("trash");
+
+
+// // afficher la quantite total d articles voulues
+// let quantityInCart = document.getElementById("quantity-in-cart");
+
+///////////////////////////////////////////
--- P5FrontEnd/showProductDetail.js
+++ P5FrontEnd/showProductDetail.js
@@ -104,28 +104,72 @@ fetch("http://localhost:3000/api/teddies/" + id)
         let idItem = btnAddToCart.dataset.id;
         let price = btnAddToCart.dataset.price;
         let firstName = btnAddToCart.dataset.name;
-
-        
-        // cardTeddy = {
-        //         id: idItem,
-        //         firstName: firstName,
-        //         price: price,
-        //         color: color,
-        //         quantity: quantity.value
-        // }
-        
+     
 
 //ENREGISTREMENT DES INFOS DANS LOCAL STORAGE AU CLICK DE L UTILISATEUR   
-        btnAddToCart.addEventListener('click', function () {
-
-            // localStorage.setItem('cardTeddy', cardTeddy)              
-            localStorage.setItem('idItem',idItem)
-            localStorage.setItem('price', price)
-            localStorage.setItem('firstname', firstName)
-            localStorage.setItem('quantity', quantity.value)
-            localStorage.setItem('color', color)
+        btnAddToCart.addEventListener('click', function(){
+            let teddyCard = {
+                idItem: idItem,
+                price: price,
+                firstName: firstName,
+                quantity: quantity.value,
+                color: color
+            }
             location.href = 'orderProduct.html'
-        })
-    });
+            addBasket(teddyCard);
+       })
+});   
 
+//FONCTIONS UTILISEES POUR LE LOCAL STORAGE
+    function initBasket() {
+        let basket = localStorage.getItem("teddyCard");            
+        if(basket != null) {
+            return JSON.parse(basket)
+        } else {
+            return [];
+        } 
+    };
+    function addBasket (product) {
+        let basket = initBasket();
+        basket.push(product);
+        localStorage.setItem("basket",JSON.stringify(basket));
+    };
 
+    // function saveShopping(product) {
+    //     localStorage.setItem('panier', JSON.stringify(panier));
+    // }   
+
+    
+/////////////////////////////////////////
+            // function CardTeddy(idItem,price,firstName,quantityValue,color) {
+            //     this.idItem = idItem;
+            //     this.price = price;
+            //     this.firstName = firstName;
+            //     this.quantityValue = quantityValue;
+            //     this.color = color;
+            // }
+
+            // function Shopping() {
+            //     this.list = [];
+            //     this.addcardTeddy = function(idItem,price,firstName,quantityValue,color) {
+            //         var index = this.getcardTeddy(idItem);
+            //         if (index == -1) this.list.push(new CardTeddy(idItem,price,firstName,quantityValue,color));
+            //         else this.list[index].quantityValue++;
+            //     }
+               
+            //     this.cardTeddy = function(idItem) {
+                
+            //         for(var i = 0 ; i <this.list.length ; i++)
+            //             if (idItem == this.list[i].getidItem()) return i;
+            //         return -1;
+            //     }
+            //     this.supprimerArticle = function(idItem){
+                
+            //         var index = this.getcardTeddy(idItem);
+            //         if (index > -1) this.list.splice(index, 1);
+            //     }
+            // }
+                
+
+         
+    
