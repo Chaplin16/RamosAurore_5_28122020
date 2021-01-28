0dc57d2e1a7a4d892d7573a16ca28acf9a8c4392
Aurore RAMOS <chaplin16@orange.fr>
2021-01-28 14:09:11 +0000

    rajout quantité panier sur chaque header

--- P5FrontEnd/orderProduct.js
+++ P5FrontEnd/orderProduct.js
@@ -4,48 +4,51 @@ const panier = JSON.parse(localStorage.getItem("basket"));
 
 //je recupere le lieu d inplantation
 let cartTableBody = document.getElementById("cart-tablebody");
-cartTableBody.innerHTML ="";
-
+cartTableBody.innerHTML = "";
+let totalQuantity = 0;
 //Condition pour afficher le panier
 if (panier) {
-    ligneTableau(); 
-    } else {
+    ligneTableau();
+} else {
     alert("le panier est vide")
 }
 
 // fonction pour supprimer une peluche   /////////////////////////A VOIR//
-
 let trash = document.getElementById("trash");  //ligne 39
-trash.addEventListener('click', function() {
-    remove({idItem:this.dataset.idItem});
+trash.addEventListener('click', function () {
+    remove({ idItem: this.dataset.idItem });
     console.log(idItem)
 });
 
- //Boucle d affichage des lignes de commande 
+//Boucle d affichage des lignes de commande 
 function ligneTableau() {
-  let total = 0;
-  let totalOrder = 0;
+    let total = 0;
+    let totalOrder = 0;
+    let quantityInCart = document.getElementById("quantity-in-cart");
+    let totalQuantity= 0;
 
     //les lignes de commande  
     panier.forEach((product) => {
-        total = product.price * product.quantity;   
+        total = product.price * product.quantity;
+        totalOrder += total
+        totalQuantity += parseInt(product.quantity);
+
         cartTableBody.innerHTML += `
                 <tr class="bg-white">
                 <td>${product.firstName}</td>
                 <td class="text-left pl-5">${product.price}€</td>
                 <td>${product.color}</td>
                 <td id="resultChoiseQuantity" class="text-left pl-5">${product.quantity}</td>
-                <td>
-                   
-                        <img id="trash" data-idItem="${product.idItem}"class="ml-4 h-75" src="trash-can_38501.png" alt="image d'une poubelle">
-                 
+                <td> 
+                    <a href="#">  
+                        <img id="trash" data-id="${product.idItem}"class="ml-4 h-75" src="trash-can_38501.png" alt="image d'une poubelle"> 
+                    </a>
                 </td>
-                <td class="text-center">${total}€</td>
-              </tr>`;  
-    }); 
-   totalPriceOrder(totalOrder);
+                <td id="calculPriceLine" class="text-center">${total}€</td>
+              </tr>`;
+    });
     // la ligne des totaux
-    cartTableBody.innerHTML += 
+    cartTableBody.innerHTML +=
         `<tr class="bg-light">
             <td>Prix total</td>
             <td></td>
@@ -54,8 +57,9 @@ function ligneTableau() {
             <td></td>
             <td class="text-center font-weight-bold">${totalOrder}€</td>
           </tr>`;
-};   
 
-function totalPriceOrder (product) {
-    
-}
+    quantityInCart.innerHTML = `<span>${totalQuantity}</span> articles`;        
+    localStorage.setItem("qtt", totalQuantity);
+};
+
+
